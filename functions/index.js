d1e177e597546c7ffd0768b79760d875079c681c
Justin Hart <justinhart90@gmail.com>
2020-08-02 14:45:04 -0600

    updated email service; resume icon tooltip

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,54 @@
-"use strict";
 // DOCS: https://firebase.google.com/docs/functions/write-firebase-functions
-exports.__esModule = true;
-var firebase_functions_1 = require("firebase-functions");
-var firebase_admin_1 = require("firebase-admin");
-var nodemailer = require("nodemailer");
-var cors = require('cors')({ origin: true });
-firebase_admin_1["default"].initializeApp();
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({origin: true});
+admin.initializeApp();
+
 /**
-* Here we're using Gmail to send
+* Here we're using Gmail to send 
 */
-var transporter = nodemailer.createTransport({
+let transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
     port: 465,
-    secure: true,
+    secure: true, 
     auth: {
-        user: firebase_functions_1["default"].config().email.account,
-        pass: firebase_functions_1["default"].config().email.pass
+        user: functions.config().email.account,
+        pass: functions.config().email.pass
     }
 });
-exports.sendMail = firebase_functions_1["default"].https.onRequest(function (req, res) {
-    cors(req, res, function () {
-        var email = req.body.email;
-        var name = req.body.name;
-        var message = req.body.message;
-        var fullMessage = name + " (" + email + "): " + message;
-        var htmlTemplate = "\n            <p style=\"font-size: 16px;\">Name: " + name + "</p>\n            <p style=\"font-size: 16px;\">Email: " + email + "</p>\n            <p style=\"font-size: 16px;\">Message:</p>\n            <p style=\"font-size: 12px;\">" + message + "</p>\n        ";
-        var mailOptions = {
-            from: email,
-            to: firebase_functions_1["default"].config().email.to,
-            subject: 'Personal Site - New Message!',
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      
+        const email = req.body.email;
+        const name = req.body.name;
+        const message = req.body.message;
+
+        const fullMessage = `${name} (${email}): ${message}`
+        const htmlTemplate = `
+            <p style="font-size: 16px;">Name: ${name}</p>
+            <p style="font-size: 16px;">Email: ${email}</p>
+            <p style="font-size: 16px;">Message:</p>
+            <p style="font-size: 12px;">${message}</p>
+        `
+
+        const mailOptions = {
+            from: email, // Something like: Jane Doe <janedoe@gmail.com>
+            to: functions.config().email.to,
+            subject: 'Personal Site - New Message!', // email subject
             text: fullMessage,
             html: htmlTemplate.toString() // email content in HTML
         };
+  
         // returning result
-        return transporter.sendMail(mailOptions, function (err) {
-            if (err) {
+        return transporter.sendMail(mailOptions, (err, info) => {
+            if(err) {
                 return res.send(err.toString());
             }
             return res.sendStatus(200);
         });
-    });
+    });    
 });
